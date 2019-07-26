efc0c7febd3c595f765968efe2bb2594233ca85c
pavankrishnan77 <pavankrishnan1993@gmail.com>
2019-07-26 19:58:08 +0530

    Function

--- src/Selenium/BrowserWindowHandleFunction.java
+++ src/Selenium/BrowserWindowHandleFunction.java
@@ -19,10 +19,31 @@ public class BrowserWindowHandleFunction
 	}
 }
 
+//Pre-Requisite:
+//Understand the above Function Logic:
+
 //Usage of above Function:
+
+//1.
+//Get the Unique Address of First Window [Window-1] and Store it in Variable Called homePage.
 //String homePage = driver.getWindowHandle();
+
+//2.
+//Call Function and Pass homePage Parameter as below and Switch to Window-2.
 //TestUtil.switchWindow(driver, homePage, homePage);
+
+//3.
+//Get the Unique Address of Second Window [Window-2] and Store it in Variable Called contactsPage.
 //String contactsPage = driver.getWindowHandle();
+
+//4.
+//Call Function and Pass homePage and contactsPage Parameters as below and Switch to Window-3.
 //TestUtil.switchWindow(driver, homePage, contactsPage);
+
+//5.
+//Once after performing actions in Window-2 and Window-3, you can switch to any Window.
 //driver.switchTo().window(homePage);
 //driver.switchTo().window(contactsPage);
+
+//6.
+//If there are 4 windows we can add one more parameter in function and follow the Same.
