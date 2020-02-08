c40adcb7672b6903dfa96107d4e42ccc8aab4493
PavanReddy77 <Pavankrishnan1993@Gmail.com>
2020-02-08 15:36:34 +0530

    Modified

--- src/Selenium/SSLCertificate.java
+++ src/Selenium/SSLCertificate.java
@@ -29,7 +29,9 @@ public static WebDriver driver;
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
+		
+		driver.get("https://www.cacert.org/");
 	}
 }
