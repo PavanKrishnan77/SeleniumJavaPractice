5ca4a4a471e3b19e3c4a55584ab1dc378ee557cd
PavanReddy77 <Pavankrishnan1993@Gmail.com>
2020-05-02 09:18:30 +0530

    Added a New Function into JavaScriptExecutorConcept

--- src/Selenium/JavaScriptExecutorConcept.java
+++ src/Selenium/JavaScriptExecutorConcept.java
@@ -11,6 +11,8 @@ import org.testng.annotations.Test;
 
 public class JavaScriptExecutorConcept 
 {
+	public static JavascriptExecutor javaScript;
+	
 	@Test
 	public void javaScriptExecutorConcept()
 	{
@@ -21,13 +23,16 @@ public class JavaScriptExecutorConcept
 		driver.manage().window().maximize(); 
 		driver.manage().deleteAllCookies(); 
 		
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		
 		driver.get("https://classic.crmpro.com/index.html");
 		
-		driver.findElement(By.name("username")).sendKeys("Reddy77077");
-		driver.findElement(By.name("password")).sendKeys("Saibaba77");
+		WebElement userName = driver.findElement(By.name("username"));
+		WebElement password = driver.findElement(By.name("password"));
+		
+		inputToTextFieldByJavaScript(userName, driver, "Reddy88077");
+		inputToTextFieldByJavaScript(password, driver, "Saibaba77");
 		
 		WebElement loginButton = driver.findElement(By.xpath("//input[@class='btn btn-small']"));
 	
@@ -47,14 +52,14 @@ public class JavaScriptExecutorConcept
 		
 		//scrollDownPageByJavaScript(driver);
 		
-		WebElement documentsLinks = driver.findElement(By.xpath("//h3[contains(text(), 'Documents')]"));
-		scrollIntoElementByJavaScript(documentsLinks, driver);
+		//WebElement documentsLinks = driver.findElement(By.xpath("//h3[contains(text(), 'Documents')]"));
+		//scrollIntoElementByJavaScript(documentsLinks, driver);
 	}
 	
 	//To Highlight WebElement by using JavaScript Executor.
 	public static void highlightElementByJavaScript(WebElement element, WebDriver driver) 
 	{
-        JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+        javaScript = ((JavascriptExecutor) driver);
         String backgroundColor = element.getCssValue("backgroundColor");
         for(int i=0; i<10; i++) 
         {
@@ -66,7 +71,7 @@ public class JavaScriptExecutorConcept
 	//To Change the Color of WebElement by using JavaScript Executor.
     public static void changeColorByJavaScript(String color, WebElement element, WebDriver driver) 
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("arguments[0].style.backgroundColor = '" + color + "'", element);
         try 
         {
@@ -81,35 +86,35 @@ public class JavaScriptExecutorConcept
     //To Draw a Border for WebElement by using JavaScript Executor.
     public static void drawElementBorderByJavaScript(WebElement element, WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("arguments[0].style.border='3px solid red'", element);
     }
     
     //To Generate an Alert by using JavaScript Executor.
     public static void generateAlertByJavaScript(WebDriver driver, String alertMessage)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("alert('" + alertMessage + "')");
     }
     
     //To Click on any WebElement by using JavaScript Executor.
     public static void clickElementByJavaScript(WebElement element, WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("arguments[0].click();", element);	
     }
-	
+    
     //To Refresh Browser by using JavaScript Executor.
     public static void refreshBrowserByJavaScript(WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("history.go(0)");
     }
     
     //To Get Title of the Page by using JavaScript Executor.
     public static void getPageTitleByJavaScript(WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	String pageTitle = javaScript.executeScript("return document.title;").toString();
     	System.out.println("The Title of the Page is ::: " +pageTitle);
     }
@@ -117,22 +122,29 @@ public class JavaScriptExecutorConcept
     //To Get the Page Inner Text by using JavaScript Executor.
     public static void getPageInnerTextByJavaScript(WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	String pageText = javaScript.executeScript("return document.documentElement.innerText;").toString();
     	System.out.println("The Text of the Page is ::: " +pageText);
     }
     
-    //To Scroll Down the Page by using JavaScript Executor..
+    //To Scroll Down the Page by using JavaScript Executor.
     public static void scrollDownPageByJavaScript(WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("window.scrollTo(0,document.body.scrollHeight)");
     }
     
-    //To Scroll into WebElement View by using JavaScript Executor..
+    //To Scroll into WebElement View by using JavaScript Executor.
     public static void scrollIntoElementByJavaScript(WebElement element, WebDriver driver)
     {
-    	JavascriptExecutor javaScript = ((JavascriptExecutor) driver);
+    	javaScript = ((JavascriptExecutor) driver);
     	javaScript.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+    
+    //To Send Input Data to Text Field by using JavaScript Executor.
+    public static void inputToTextFieldByJavaScript(WebElement element, WebDriver driver, String data)
+    {
+    	javaScript = ((JavascriptExecutor) driver);
+    	javaScript.executeScript("arguments[0].value='" + data + "'", element);
     }
 }
